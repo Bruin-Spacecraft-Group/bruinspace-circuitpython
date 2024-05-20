2a8f835393f1fe87508fc1ccbf29a91209c7b2f9
Annie Xiang <anniexiang01@gmail.com>
2024-05-20 00:29:59 -0700

    added board configurations for RAPID-0 CDH, ADCS, and EPS boards. reverted Metro M4 Express board configurations to original

--- ports/atmel-samd/boards/rapid0_eps/mpconfigboard.h
+++ ports/atmel-samd/boards/rapid0_eps/mpconfigboard.h
@@ -1,12 +1,4 @@
-// This file is part of the CircuitPython project: https://circuitpython.org
-//
-// SPDX-FileCopyrightText: Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
-//
-// SPDX-License-Identifier: MIT
-
-#pragma once
-
-#define MICROPY_HW_BOARD_NAME "Adafruit Metro M4 Express"
+#define MICROPY_HW_BOARD_NAME "RAPID-0 EPS"
 #define MICROPY_HW_MCU_NAME "samd51j19"
 
 #define CIRCUITPY_MCU_FAMILY samd51
@@ -20,7 +12,7 @@
 
 #define MICROPY_HW_NEOPIXEL (&pin_PB22)
 
-#define BOARD_HAS_CRYSTAL 1
+#define BOARD_HAS_CRYSTAL 0
 
 #define DEFAULT_I2C_BUS_SCL (&pin_PB03)
 #define DEFAULT_I2C_BUS_SDA (&pin_PB02)
@@ -32,21 +24,6 @@
 #define DEFAULT_UART_BUS_RX (&pin_PA23)
 #define DEFAULT_UART_BUS_TX (&pin_PA22)
 
-// Used for 32 kHz crystal
-#define IGNORE_PIN_PA00     1
-#define IGNORE_PIN_PA01     1
-
 // USB is always used internally so skip the pin objects for it.
 #define IGNORE_PIN_PA24     1
 #define IGNORE_PIN_PA25     1
-
-// Not connected
-#define IGNORE_PIN_PA07     1
-#define IGNORE_PIN_PA15     1
-#define IGNORE_PIN_PB00     1
-#define IGNORE_PIN_PB01     1
-#define IGNORE_PIN_PB04     1
-#define IGNORE_PIN_PB05     1
-#define IGNORE_PIN_PB23     1
-#define IGNORE_PIN_PB30     1
-#define IGNORE_PIN_PB31     1
--- ports/atmel-samd/boards/rapid0_eps/mpconfigboard.mk
+++ ports/atmel-samd/boards/rapid0_eps/mpconfigboard.mk
@@ -1,28 +1,20 @@
 USB_VID = 0x239A
 USB_PID = 0x8021
-USB_PRODUCT = "Metro M4 Express"
-USB_MANUFACTURER = "Adafruit Industries LLC"
+USB_PRODUCT = "RAPID-0 EPS"
+USB_MANUFACTURER = "Bruin Spacecraft Group"
 
 CHIP_VARIANT = SAMD51J19A
 CHIP_FAMILY = samd51
 
 QSPI_FLASH_FILESYSTEM = 1
-EXTERNAL_FLASH_DEVICES = "S25FL116K, S25FL216K, GD25Q16C, W25Q16JVxQ"
+EXTERNAL_FLASH_DEVICES = "GD25Q16C"
 LONGINT_IMPL = MPZ
 
 CIRCUITPY__EVE = 1
-<<<<<<< HEAD
-<<<<<<< HEAD
-CIRCUITPY_CODEOP = 0
-=======
 CIRCUITPY_CANIO = 0
->>>>>>> working edits to spi and can attempts
-=======
->>>>>>> added board configurations for RAPID-0 CDH, ADCS, and EPS boards. reverted Metro M4 Express board configurations to original
 CIRCUITPY_FLOPPYIO = 0
 CIRCUITPY_JPEGIO = 0
 CIRCUITPY_SYNTHIO = 0
-CIRCUITPY_VECTORIO = 0
 
 # We don't have room for the fonts for terminalio for certain languages,
 # so turn off terminalio, and if it's off and displayio is on,
