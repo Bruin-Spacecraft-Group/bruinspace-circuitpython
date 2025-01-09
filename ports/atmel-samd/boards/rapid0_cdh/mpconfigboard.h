ae58f9f4000445d5143a4ebcba96eca145ae5fa3
Randall Scharpf <randall.scharpf@gmail.com>
2025-01-09 11:04:01 -0800

    sigma yellow

--- ports/atmel-samd/boards/rapid0_cdh/mpconfigboard.h
+++ ports/atmel-samd/boards/rapid0_cdh/mpconfigboard.h
@@ -10,13 +10,16 @@
 
 // #define MICROPY_HW_NEOPIXEL (&pin_PB22)
 
-#define SPI_FLASH_MOSI_PIN          &pin_PA08
-#define SPI_FLASH_MISO_PIN          &pin_PA09
-#define SPI_FLASH_SCK_PIN           &pin_PB10
-#define SPI_FLASH_CS_PIN            &pin_PB11
+// #define SPI_FLASH_MOSI_PIN          &pin_PA08
+// #define SPI_FLASH_MISO_PIN          &pin_PA09
+// #define SPI_FLASH_SCK_PIN           &pin_PB10
+// #define SPI_FLASH_CS_PIN            &pin_PB11
 
 #define BOARD_HAS_CRYSTAL 0
 
 // USB is always used internally so skip the pin objects for it.
 #define IGNORE_PIN_PA24     1
 #define IGNORE_PIN_PA25     1
+
+#define CIRCUITPY_INTERNAL_FLASH_FILESYSTEM_SIZE (64)
+#define CIRCUITPY_INTERNAL_NVM_SIZE (64)
