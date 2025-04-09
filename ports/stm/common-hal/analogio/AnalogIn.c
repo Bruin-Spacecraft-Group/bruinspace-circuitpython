4d9a14f2f5b818539fdfe7bf4f8873801306dfe0
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-08 21:10:43 -0700

    idk anymore

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -192,9 +192,8 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
 
   if (HAL_ADC_Init(&AdcHandle) != HAL_OK)
   {
-    uint32_t error = HAL_ADC_GetError(&AdcHandle);
-    mp_raise_msg_varg(&mp_type_RuntimeError, "ADC initialization failed (error: 0x%lx)", error);
-    return 0;
+    mp_raise_msg_varg(&mp_type_RuntimeError, "ADC failed: 0x%lx", HAL_ADC_GetError(&AdcHandle));
+    return mp_const_none;
   }
 
   /** Configure the ADC multi-mode
