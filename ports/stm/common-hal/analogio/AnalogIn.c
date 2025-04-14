d8dc00dc1bb4308171eb1082e413827740d929fd
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-14 12:19:25 -0700

    alternate hal name for adc clk enable

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -61,7 +61,7 @@ void common_hal_analogio_analogin_construct(analogio_analogin_obj_t *self,
     common_hal_mcu_pin_claim(pin);
     self->pin = pin;
     #if CPY_STM32H7
-    __HAL_RCC_ADC1_CLK_ENABLE(); 
+    __ADC_CLK_ENABLE(); 
     RCC_PeriphClkInitTypeDef RCC_PeriphClkInit;
     RCC_PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
     RCC_PeriphClkInit.AdcClockSelection = RCC_ADCCLKSOURCE_PLL2;
