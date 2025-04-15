edff60ab6aaaa2c5aae1a2ad44347469cf9d6cf2
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-14 18:53:35 -0700

    AdcHandle def in getval func

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -141,7 +141,7 @@ void common_hal_analogio_analogin_construct(analogio_analogin_obj_t *self,
     uint32_t error = HAL_ADC_Init(&AdcHandle);
     mp_printf(&mp_plat_print, "ADC Error: 0x%lX\n", error);
     mp_raise_RuntimeError(MP_ERROR_TEXT("1"));
-    // return 0;
+    return;
   }
 
   /** Configure the ADC multi-mode
@@ -165,7 +165,7 @@ void common_hal_analogio_analogin_construct(analogio_analogin_obj_t *self,
   if (HAL_ADC_ConfigChannel(&AdcHandle, &sConfig) != HAL_OK)
   {
     mp_raise_RuntimeError(MP_ERROR_TEXT("3"));
-    // return 0;
+    return;
   }
 }
 
@@ -233,25 +233,25 @@ uint32_t adc_channel(uint32_t channel) {
 uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
     // Something else might have used the ADC in a different way,
     // so we completely re-initialize it.
-//     ADC_TypeDef *ADCx;
+    ADC_TypeDef *ADCx;
 
-//     if (self->pin->adc_unit & 0x01) {
-//         ADCx = ADC1;
+    if (self->pin->adc_unit & 0x01) {
+        ADCx = ADC1;
 //         #if CPY_STM32L4
 //         __HAL_RCC_ADC_CLK_ENABLE();
 //         #elif CPY_STM32H7
 //         __HAL_RCC_ADC12_CLK_ENABLE();
 //         #endif
-//     } else if (self->pin->adc_unit == 0x04) {
-//         #ifdef ADC3
-//         ADCx = ADC3;
-//         #endif
+    } else if (self->pin->adc_unit == 0x04) {
+        #ifdef ADC3
+        ADCx = ADC3;
+        #endif
 //         #if CPY_STM32H7
 //         __HAL_RCC_ADC3_CLK_ENABLE();
 //         #endif
-//     } else {
-//         mp_raise_RuntimeError(MP_ERROR_TEXT("Invalid ADC Unit value"));
-//     }
+    } else {
+        mp_raise_RuntimeError(MP_ERROR_TEXT("Invalid ADC Unit value"));
+    }
 
 //     #if CPY_STM32H7
 //     RCC_PeriphCLKInitTypeDef RCC_PeriphClkInit;
