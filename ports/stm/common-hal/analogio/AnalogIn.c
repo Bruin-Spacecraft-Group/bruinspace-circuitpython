1f573c50bdffc49ae5db9ff5d775c197d5d6dc74
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-15 11:36:02 -0700

    add back adc multi-mode

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -159,7 +159,7 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
 
 //     // HAL Implementation
     ADC_HandleTypeDef AdcHandle = {};
-    // ADC_MultiModeTypeDef multimode = {0};
+    ADC_MultiModeTypeDef multimode = {0};
   ADC_ChannelConfTypeDef sConfig = {0};
 
   /** Common config
@@ -210,12 +210,12 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
 
   /** Configure the ADC multi-mode
   */
-//   multimode.Mode = ADC_MODE_INDEPENDENT;
-//   if (HAL_ADCEx_MultiModeConfigChannel(&AdcHandle, &multimode) != HAL_OK)
-//   {
-//     mp_raise_RuntimeError(MP_ERROR_TEXT("2"));
-//     return 0;
-//   }
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&AdcHandle, &multimode) != HAL_OK)
+  {
+    mp_raise_RuntimeError(MP_ERROR_TEXT("2"));
+    return 0;
+  }
 
   /** Configure Regular Channel
   */
