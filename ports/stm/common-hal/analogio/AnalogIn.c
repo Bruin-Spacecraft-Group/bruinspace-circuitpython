6283d15618608c75030a80859c82b999e4365e99
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-15 23:33:46 -0700

    adc calibration parameters

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -219,7 +219,7 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
   {
     return 0;
   }
-  HAL_ADCEx_Calibration_Start();
+  HAL_ADCEx_Calibration_Start(&AdcHandle, ADC_CALIB_OFFSET, sConfig.SingleDiff);
     if (HAL_ADC_Start(&AdcHandle) != HAL_OK) {
         return 0;
     }
