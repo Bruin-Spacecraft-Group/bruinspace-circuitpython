9c9043b1aeaaa43eb043d180ba123a175b68d16c
AstroPancake72 <owen.t.rusk@gmail.com>
2025-04-16 21:25:26 -0700

    adding parentheses

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -227,7 +227,7 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
     HAL_ADC_Stop(&AdcHandle);
 
     // Stretch 12-bit ADC reading to 16-bit range
-    #if AdcHandle.Init.Resolution == ADC_RESOLUTION_12B
+    #if (AdcHandle.Init.Resolution == ADC_RESOLUTION_12B)
     return (value << 4) | (value >> 8);
     #else
     return value;
