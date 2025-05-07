b5126332aaf3f2651acab55c5816d01a7007f4c9
AstroPancake72 <owen.t.rusk@gmail.com>
2025-05-06 22:30:11 -0700

    setting sconfig channel correctly

--- ports/stm/common-hal/analogio/AnalogIn.c
+++ ports/stm/common-hal/analogio/AnalogIn.c
@@ -200,7 +200,7 @@ uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
     /** Configure Regular Channel
     */
 
-    sConfig.Channel = ADC_CHANNEL_3;
+    sConfig.Channel = self->pin->p_adc->adc_channel;
     sConfig.Rank = ADC_REGULAR_RANK_1;
     sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
     sConfig.SingleDiff = ADC_SINGLE_ENDED;
