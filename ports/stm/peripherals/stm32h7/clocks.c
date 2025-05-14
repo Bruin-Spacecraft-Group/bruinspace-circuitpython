1ba5c8776243b911f88f82ac0226bacc0d949587
AstroPancake72 <owen.t.rusk@gmail.com>
2025-05-13 19:25:44 -0700

    periphclck struct for pll2

--- ports/stm/peripherals/stm32h7/clocks.c
+++ ports/stm/peripherals/stm32h7/clocks.c
@@ -97,14 +97,14 @@ void stm32_peripherals_clocks_init(void) {
     // ADC Clock init
     PeriphClkInitStruct.AdcClockSelection = RCC_ADCCLKSOURCE_PLL2;
 
-    RCC_OscInitStruct.PLL2.PLL2M = 1;
-    RCC_OscInitStruct.PLL2.PLL2N = 12;
-    RCC_OscInitStruct.PLL2.PLL2P = 2;
-    RCC_OscInitStruct.PLL2.PLL2Q = 4;
-    RCC_OscInitStruct.PLL2.PLL2R = 2;
-    RCC_OscInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_1;
-    RCC_OscInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;;
-    RCC_OscInitStruct.PLL2.PLL2FRACN = 0;
+    PeriphClkInitStruct.PLL2.PLL2M = 1;
+    PeriphClkInitStruct.PLL2.PLL2N = 12;
+    PeriphClkInitStruct.PLL2.PLL2P = 2;
+    PeriphClkInitStruct.PLL2.PLL2Q = 4;
+    PeriphClkInitStruct.PLL2.PLL2R = 2;
+    PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_1;
+    PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;;
+    PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
 
     #ifdef STM32H750xx
     // USB
