305e47d34e10d41f548119619ecdc03ac6069d62
AstroPancake72 <owen.t.rusk@gmail.com>
2025-05-19 09:44:57 -0700

    stuck in infinite loop from pll2 check

--- ports/stm/peripherals/stm32h7/clocks.c
+++ ports/stm/peripherals/stm32h7/clocks.c
@@ -103,10 +103,6 @@ void stm32_peripherals_clocks_init(void) {
     PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
     PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
 
-    __HAL_RCC_PLL2_ENABLE();
-    while (__HAL_RCC_GET_FLAG(RCC_FLAG_PLL2RDY) == RESET) {
-    // Wait for PLL2 to stabilize
-    }
 
     // ADC Clock init
     PeriphClkInitStruct.AdcClockSelection = RCC_ADCCLKSOURCE_PLL2;
@@ -135,4 +131,8 @@ void stm32_peripherals_clocks_init(void) {
     }
     // Enable USB Voltage detector
     HAL_PWREx_EnableUSBVoltageDetector();
+
+    while (__HAL_RCC_GET_FLAG(RCC_FLAG_PLL2RDY) == RESET) {
+    // Wait for PLL2 to stabilize
+    }
 }
